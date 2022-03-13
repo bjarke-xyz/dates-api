ea3fd404856889577f4fab680c57ced626aec55a
bjarkt <688414+bjarkt@users.noreply.github.com>
2022-03-13 18:18:38 +0100

    cors, delete route

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,21 @@ router.post('/naturaldate/parse', async (request: Request) => {
   }
 
   const parsedDate = parseDate(body.naturalDate)
-  const response = {
+  const responseObj = {
     input: body.naturalDate,
     output: parsedDate?.toISOString() ?? null,
   }
-  return new Response(JSON.stringify(response), {
+  const response = new Response(JSON.stringify(responseObj), {
     headers: {
       'Content-Type': 'application/json',
     },
   })
+
+  // Set CORS headers
+  const url = new URL(request.url)
+  response.headers.set('Access-Control-Allow-Origin', '*')
+
+  return response
 })
 
 router.get('/datetime/utcnow', (request) => {
@@ -38,6 +44,51 @@ router.get('/datetime/utcnow', (request) => {
   })
 })
 
+router.options('*', (request: Request) => {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    // Cloudflare supports the GET, POST, HEAD, and OPTIONS methods from any origin,
+    // and allow any header on requests. These headers must be present
+    // on all responses to all CORS preflight requests. In practice, this means
+    // all responses to OPTIONS requests.
+    const corsHeaders = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    }
+
+    let respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers': request.headers.get(
+        'Access-Control-Request-Headers',
+      ),
+    }
+
+    return new Response(null, {
+      headers: respHeaders,
+    })
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    })
+  }
+})
+
 router.all('*', () => {
   return new Response('Not found', {
     status: 404,
@@ -45,7 +96,5 @@ router.all('*', () => {
 })
 
 export default {
-  async fetch(request: Request) {
-    return router.handle(request)
-  },
+  fetch: router.handle,
 }
